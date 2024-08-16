5dd0d99dfca6721ae46435ef2fbf0ef57dbd728f
GordonOchieng <gordonochieng5454@gmail.com>
2024-08-16 18:12:51 +0300

    added lines of code on FibSolver module

---
+++ lib/fibonacci.ex
@@ -0,0 +1,18 @@
+defmodule Fibonacci do
+  @moduledoc """
+  Documentation for `Fibonacci`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Fibonacci.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
